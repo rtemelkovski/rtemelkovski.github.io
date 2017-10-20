31dc04ab76959fd4228c0aa0b5790d1da3c88483
Roberto Temelkovski <rtemelkovski@yahoo.com>
2017-10-20 16:28:55 -0400

    site fixed

--- titlescript.js
+++ titlescript.js
@@ -8,7 +8,6 @@ var descriptions = [
 
 function loadNext(cur){
 	setTimeout(function(){ 
-		console.log('here');
 		var elem = document.getElementById("alterable-subtitle");
 		elem.innerHTML = descriptions[cur];
 		var next = (cur + 1) % descriptions.length;
